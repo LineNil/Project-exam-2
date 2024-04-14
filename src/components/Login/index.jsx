7c0e9c8dfa4bc690846f997d70a7b9a300733337
Line Nilsen <lineforslund@hotmail.com>
2024-04-14 20:17:28 +0200

    login

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -4,9 +4,28 @@ import React from "react";
 function LogIn(){
   return(
     <div>
-      <p>Log in page</p>
-    </div>
-  );
+    <h2>Log in</h2>
+    <form>
+      <label>
+        Email:
+        <input
+          type="email"
+          name="email"
+          required
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          name="password"
+          required
+        />
+      </label>
+      <button type="submit">Log In</button>
+    </form>
+  </div>
+);
 }
 
 export default LogIn
