94388e95dab4fd10a1feff63c49a4d239bea43a3
Line Nilsen <lineforslund@hotmail.com>
2024-04-14 20:40:17 +0200

    Initial commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,19 +10,19 @@ import ManagerPage from "../ManagerPage";
 function App(){
   return(
     <BrowserRouter>
-        <div>
+      <div>
       <Layout>
-        <Routes>
-        <Route path="/" element={<VenueList/>} />
+          <Routes>
+            <Route path="/" element={<VenueList/>} />
         <Route path="/login" element={<LogIn/>} />
         <Route path="/register" element={<Register/>} /> 
+
+            <Route path="/manager" element={<ManagerPage />} />
+            <Route path="/user-account" element={<UserAccount />} />
+          </Routes>
         
-        <Route path="/manager" element={<ManagerPage />} />
-        <Route path="/user-account" element={<UserAccount />} />
-        </Routes>
-        
-      </Layout>
-    </div>
+        </Layout>
+      </div>
     </BrowserRouter>
 
   );
